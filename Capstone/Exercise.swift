a12f5e02e5a0d51c680081698412f8460e429177
reddy88 <reddymithun88@gmail.com>
2017-09-11 14:43:35 -0600

    Working copy

--- Capstone/Exercise.swift
+++ Capstone/Exercise.swift
@@ -9,47 +9,45 @@
 import Foundation
 import CoreData
 
-extension Exercise {
+class Exercise {
     
     // MARK: - Class properties
     
-//    private let nameKey = "name"
-//    private let categoryKey = "category"
-//    private let idKey = "id"
+    private let nameKey = "name"
+    private let categoryKey = "category"
+    private let idKey = "id"
     
     // MARK: - Instance Properties
     
-//    let name: String
-//    let category: Int
-//    let id: Int
-//    var isSelected: Bool = false
+    let name: String
+    let category: Int
+    let id: Int
+    var isSelected: Bool = false
     
     // MARK: - Initializers
     
-    convenience init(name: String, category: Int, id: Int, context: NSManagedObjectContext = CoreDataStack.context) {
-        self.init(context: context)
-        
+    init(name: String, category: Int, id: Int) {
         self.name = name
-        self.category = Int16(category)
-        self.id = Int16(id)
+        self.category = category
+        self.id = id
     }
     
-    convenience init?(dictionary: [String: Any]) {
+    init?(dictionary: [String: Any]) {
         guard let name = dictionary["name"] as? String,
             let category = dictionary["category"] as? Int,
             let id = dictionary["id"] as? Int else { return nil }
         
         self.name = name
-        self.category = Int16(category)
-        self.id = Int16(id)
+        self.category = category
+        self.id = id
     }
     
 }
 
 // MARK: Equatable
 
-//extension Exercise: Equatable {
-//    static func ==(lhs: Exercise, rhs: Exercise) -> Bool {
-//        return lhs.id == rhs.id
-//    }
-//}
+extension Exercise: Equatable {
+    static func ==(lhs: Exercise, rhs: Exercise) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
